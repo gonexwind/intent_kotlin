93401e590d8fc88f9f6f7b9e21c891097b511e0f
gonexwind <fikky.ardi15@gmail.com>
2021-07-20 21:38:45 +0700

    move activity

---
+++ app/src/main/java/com/fikkyardianto/intentkotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.fikkyardianto.intentkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveAcivity: Button = findViewById(R.id.btn_move_activity)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+}
